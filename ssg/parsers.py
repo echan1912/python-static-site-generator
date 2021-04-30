7e6b6b583fa9d783ccc03ab3456d67f02c0bef1d
echan1912 <echan1912@gmail.com>
2021-04-30 11:25:28 +0800

    check

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -31,7 +31,7 @@ class ResourceParser(Parser):
     def parse(self, path, source, dest):
         self.copy(path, source, dest)
 
-class MarkdownParser():
+class MarkdownParser(Parser):
     extensions = [".md", ".markdown"]
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
@@ -39,13 +39,13 @@ class MarkdownParser():
         self.write(path, dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n")
 
-class RestructuredTextParser():
+class RestructuredTextParser(Parser):
     extensions = [".rst"]
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name = "html5")
         self.write(path, dest, html["html_body"])
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
-        
+
         
 
